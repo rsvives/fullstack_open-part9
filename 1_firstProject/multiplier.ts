cf2d3141def5ed23bfc1509adda5325e0f9ad341
Rodri <r.serranvives@gmail.com>
2024-09-10 23:41:14 +0200

    refactor

--- 1_firstProject/multiplier.ts
+++ 1_firstProject/multiplier.ts
@@ -6,7 +6,16 @@ const multiplicator = (a:number,b:number,op:Operation) => {
     if(op === 'divide'){
         if(b === 0) throw new Error('Can\'t divide by 0!')
         return a/b
+    }else{
+        throw Error('Unknown operation')
     }
 }
-
-multiplicator(5, 8, 'multiply');
+try{
+    multiplicator(5, 8, 'multiply');
+}catch(error:unknown){
+    let errorMessage = 'Something went wrong. '
+    if(error instanceof Error){
+        errorMessage+= error.message
+    }
+    console.error(error)
+}
