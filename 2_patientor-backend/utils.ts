d7abbb2e3f40c8c91cd3439783abf7ce54cb06f1
Rodri <r.serranvives@gmail.com>
2024-09-17 12:20:34 +0200

    zod object schema

--- 2_patientor-backend/utils.ts
+++ 2_patientor-backend/utils.ts
@@ -1,60 +1,15 @@
+import {z} from 'zod';
 import { Gender, NewPatient } from './types';
 
+
+const newPatientSchema = z.object({
+    name:z.string(),
+    dateOfBirth: z.string().date(),
+    occupation: z.string(),
+    ssn: z.string(),
+    gender: z.nativeEnum(Gender)
+});
+
 export const newPatientConverter = (object:unknown):NewPatient=>{
-    if ( !object || typeof object !== 'object' ) {
-        throw new Error('Incorrect or missing data');
-    }
-    
-    if('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'occupation' in object && 'gender' in object){
-
-        const newPatient:NewPatient = {
-            name:parseName(object.name),
-            dateOfBirth:parseDateOfBirth(object.dateOfBirth),
-            ssn:parseSsn(object.ssn),
-            gender:parseGender(object.gender),
-            occupation:parseOccupation(object.occupation)
-        };
-        return newPatient;
-    }
-    throw new Error('Incorrect data: some fields are missing');
+    return newPatientSchema.parse(object);
 };
-export const parseName = (name:unknown):string=>{
-    if (!name || !isString(name)) {
-        throw new Error('Incorrect or missing name');
-      }
-      return name;
-};
-export const parseDateOfBirth = (dateOfBirth:unknown):string=>{
-    if (!dateOfBirth || !isString(dateOfBirth) || !isDate(dateOfBirth)) {
-        throw new Error('Incorrect or missing dateOfBirth');
-      }
-      return dateOfBirth;
-};
-export const parseSsn = (ssn:unknown):string=>{
-    if (!ssn || !isString(ssn)) {
-        throw new Error('Incorrect or missing ssn');
-      }
-      return ssn;
-};
-export const parseOccupation = (occupation:unknown):string=>{
-    if (!occupation || !isString(occupation)) {
-        throw new Error('Incorrect or missing occupation');
-      }
-      return occupation;
-};
-export const parseGender = (gender:unknown):Gender=>{
-    if (!gender || !isString(gender) || !isGender(gender)) {
-        throw new Error('Incorrect or missing gender');
-      }
-      return gender;
-};
-
-const isGender = (param:string): param is Gender=>{
-    return Object.values(Gender).map(g=>g.toString()).includes(param);
-};
-const isDate = (date:string):boolean =>{
-    return Boolean(Date.parse(date));
-};
-const isString = (text: unknown): text is string => {
-    return typeof text === 'string' || text instanceof String;
-  };
