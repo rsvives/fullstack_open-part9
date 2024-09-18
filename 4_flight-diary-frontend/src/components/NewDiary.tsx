ca11cea7578e6780f049734e9b34c962f0c45028
Rodri <r.serranvives@gmail.com>
2024-09-18 20:07:10 +0200

    ex 9.20 - radio buttons & input date

--- 4_flight-diary-frontend/src/components/NewDiary.tsx
+++ 4_flight-diary-frontend/src/components/NewDiary.tsx
@@ -1,20 +1,21 @@
+import { useState } from "react"
 import { useField } from "../hooks"
 import { NewDiaryEntry, Visibility, Weather } from "../types"
 
 const NewDiary = ({handleSubmit}:{handleSubmit:(newDiaryEntry:NewDiaryEntry)=>void})=>{
-    const dateInput = useField('text')
-    const visibilityInput = useField('text')
-    const weatherInput = useField('text')
+    const dateInput = useField('date')
+    const [visibility,setVisibility] = useState('')
+    const [weather,setWeather] = useState('')
     const commentInput = useField('text')
   
     const diaryCreation = async(event: React.SyntheticEvent)=>{
     event.preventDefault()
-    console.log(dateInput.input.value,visibilityInput.input.value,weatherInput.input.value,commentInput.input.value)
+    console.log(dateInput.input.value,visibility,weather,commentInput.input.value)
   
     const newDiary = {
       date:dateInput.input.value, 
-      visibility:visibilityInput.input.value as Visibility,
-      weather:weatherInput.input.value as Weather,
+      visibility:visibility as Visibility,
+      weather:weather as Weather,
       comment:commentInput.input.value,
     }
     handleSubmit(newDiary)
@@ -26,13 +27,30 @@ const NewDiary = ({handleSubmit}:{handleSubmit:(newDiaryEntry:NewDiaryEntry)=>vo
     <h2>New diary</h2>
       <form onSubmit={diaryCreation} >
         <label htmlFor="date-input">Date:</label><br />
-        <input id="date-input" {...dateInput.input} /><br /><br />
+        <input id="date-input" {...dateInput.input} required/><br /><br />
         
-        <label htmlFor="visibility-input">Visibility:</label><br />
-        <input id="visibility-input" {...visibilityInput.input}/><br /><br />
         
-        <label htmlFor="weather-input">Weather:</label><br />
-        <input id="weather-input" {...weatherInput.input}/><br /><br />
+        <legend>Visibility:</legend>
+        {Object.values(Visibility).map(el=>{
+            return(
+                <>
+                <input id={el+'-visibility-input'} type="radio" value={el} name="visibility" onChange={()=>setVisibility(el)}/>
+                <label htmlFor={el+'-visibility-input'}>{el}</label>
+            </>
+            )
+        })}
+        <br /><br />
+
+        <legend>Weather:</legend>
+        {Object.values(Weather).map(el=>{
+            return(
+            <>
+                <input id={el+'-weather-input'} type="radio" value={el} name="weather" onChange={()=>setWeather(el)}/>
+                <label htmlFor={el+'-weather-input'}>{el}</label>
+            </>
+            )
+        })}
+        <br /><br />
         
         <label htmlFor="comment-input">Comment:</label><br />
         <input id="comment-input" {...commentInput.input}/><br /><br />
