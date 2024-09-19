0756966f2815f95ff49ef630db3a6084314a0b1c
Rodri <r.serranvives@gmail.com>
2024-09-19 16:36:33 +0200

    ex 9.23 - entries in backend

--- 2_patientor-backend/types.ts
+++ 2_patientor-backend/types.ts
@@ -1,12 +1,6 @@
 import { z } from "zod";
 
-export interface Entry{
-    description:string;
-    creation: string;
-    specialist: string;
-    diagnosis: string[];
 
-}
 
 export type Diagnosis = {
     code: string,
@@ -25,13 +19,61 @@ export const NewPatientSchema = z.object({
     dateOfBirth: z.string().date(),
     occupation: z.string(),
     ssn: z.string(),
-    gender: z.nativeEnum(Gender)
+    gender: z.nativeEnum(Gender),
+    // entries: Entry[]
 });
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
 export interface Patient extends NewPatient  {
     id:          string;
-    entries: Entry[]
+    entries:     Entry[]
 };
 
 export type PatientWithoutSSN = Omit<Patient,'ssn' | 'entries'>;
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+  }
+
+  export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+  
+  interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+  }
+type Discharge = {
+    date: string,
+    criteria: string
+};
+interface HospitalEntry extends BaseEntry{
+    type: 'Hospital';
+    discharge: Discharge;
+}
+type SickLeave ={
+    startDate: string,
+    endDate: string
+};
+interface OccupationalHealthcareEntry extends BaseEntry{
+    type:'OccupationalHealthcare';
+    employerName:string;
+    sickLeave: SickLeave;
+};
+
+  export type Entry =
+   HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// // Define special omit for unions
+// type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// // Define Entry without the 'id' property
+// type EntryWithoutId = UnionOmit<Entry, 'id'>;
